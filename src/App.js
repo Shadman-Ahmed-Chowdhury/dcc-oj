48189da6068fdfec2279026973b0bfd9f42ed3ad
Shadman-Ahmed-Chowdhury <shadmanchowdhury87@gmail.com>
2021-01-02 14:29:36 +0600

    Home Page and navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+//Importing Global CSS File
 import "./App.css";
+
+//Importing Components
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello React world!</h1>
+    <div>
+      <Router>
+        <div>
+          <Route path="/" exact component={Home} />
+          <Route path="/login" exact component={Login} />
+        </div>
+      </Router>
     </div>
   );
 }
