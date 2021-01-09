c4266fc102e8d743408b68d3920c2bde20c896d5
Shadman-Ahmed-Chowdhury <shadmanchowdhury87@gmail.com>
2021-01-09 08:29:36 +0600

    problem tags added in the form

--- src/app-logic/saveProblem.js
+++ src/app-logic/saveProblem.js
@@ -1,56 +1,58 @@
-import firestore from './firestore';
+import firestore from "./firestore";
 
 //Importing SweetAlert
-import Swal from 'sweetalert2';
-import withReactContent from 'sweetalert2-react-content';
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 
 const saveProblem = (
-	id,
-	title,
-	difficulty,
-	description,
-	input,
-	constraints,
-	output,
-	sampleInput,
-	sampleOutput,
-	testCaseInput,
-	testCaseOutput,
-	problemSetter
+  id,
+  title,
+  difficulty,
+  description,
+  input,
+  constraints,
+  output,
+  sampleInput,
+  sampleOutput,
+  testCaseInput,
+  testCaseOutput,
+  problemSetter,
+  tags
 ) => {
-	const MySwal = withReactContent(Swal);
-	MySwal.showLoading();
-	firestore
-		.collection('Problems')
-		.doc(id)
-		.set({
-			id,
-			title,
-			difficulty,
-			description,
-			input,
-			constraints,
-			output,
-			sampleInput,
-			sampleOutput,
-			testCaseInput,
-			testCaseOutput,
-			problemSetter,
-			totalSubmissions: 0,
-			totalAcceptedSubmissions: 0,
-		})
-		.then(() => {
-			MySwal.fire({
-				icon: 'success',
-				title: 'Data Saved!',
-				confirmButtonText: 'Okay',
-			}).then(() => {
-				window.location.assign('/');
-			});
-		})
-		.catch((error) => {
-			console.log(error);
-		});
+  const MySwal = withReactContent(Swal);
+  MySwal.showLoading();
+  firestore
+    .collection("Problems")
+    .doc(id)
+    .set({
+      id,
+      title,
+      difficulty,
+      description,
+      input,
+      constraints,
+      output,
+      sampleInput,
+      sampleOutput,
+      testCaseInput,
+      testCaseOutput,
+      problemSetter,
+      tags,
+      totalSubmissions: 0,
+      totalAcceptedSubmissions: 0,
+    })
+    .then(() => {
+      MySwal.fire({
+        icon: "success",
+        title: "Data Saved!",
+        confirmButtonText: "Okay",
+      }).then(() => {
+        window.location.assign("/");
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 };
 
 export default saveProblem;
